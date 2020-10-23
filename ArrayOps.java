ce4fb8e64ab47ceb05c716f6fe7945224b23be9b
sookiemonster <dsooknanan20@stuy.edu>
2020-10-23 12:06:09 -0400

    Added sum method & test for that method

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,2 +1,9 @@
 public class ArrayOps {
+  public static int sum(int[] arr) {
+    int result = 0;
+    for (int i = 0; i < arr.length; i++) {
+      result += arr[i];
+    }
+    return result;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester {
   public static void main(String[] args) {
-    
+    int[] arr = new int[] {12, -2, 3};
+    System.out.println( ArrayOps.sum(arr) );
   }
 }
